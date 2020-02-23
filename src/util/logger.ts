81bdb90aaf6577c807cd782c1d4860ec4d195f94
Ubuntu <iot@IOT-SIGNAL-WS-NODE-DEV.y1v1qkmus3ueld0voo3hqzl5oe.cx.internal.chinacloudapp.cn>
2020-02-23 16:28:03 +0000

    fix missing level

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -11,7 +11,8 @@ var fileRotateTransport = new DailyRotateFile({
     maxSize: '20m',
     maxFiles: '100',
     utc: true,
-    dirname: './logs'
+    dirname: './logs',
+    level:"debug"
   });
 
 const options: winston.LoggerOptions = {
